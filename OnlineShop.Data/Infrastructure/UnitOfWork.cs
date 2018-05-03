2f7e00de3caa395992bbdcd0209f2affb51b937e
Vu Viet Ha <havv56@gmail.com>
2018-05-03 15:29:20 +0700

    Add db entities framework code first for data

---
+++ OnlineShop.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿namespace OnlineShop.Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbFactory dbFactory;
+        private OnlineShopDbContext dbContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+
+        public OnlineShopDbContext DbContext
+        {
+            get { return dbContext ?? (dbContext = dbFactory.Init()); }
+        }
+
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+}
