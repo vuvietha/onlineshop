881d4e5d5b6a25a819049a285f38e1d5fb2cca95
Vu Viet Ha <havv56@gmail.com>
2018-05-07 13:58:02 +0700

    add service in angularjs

--- OnlineShop.Web/Scripts/spa/app.js
+++ OnlineShop.Web/Scripts/spa/app.js
@@ -5,22 +5,46 @@ var myApp = angular.module('myModule', []);
 
 //register the controller with the module
 myApp.controller("schoolController", schoolController);
-myApp.controller("studentController", studentController);
-myApp.controller("teacherController", teacherController);
-
+myApp.service("Validator", Validator)
 //myApp.controller("myController", function ($scope) { $scope.message = "This is my message from controller"; });
 
 //inject $scope into myController like autofac mechanism in backend
 //myController.$inject = ['$scope'];
 
+schoolController.$inject = ['$scope', 'Validator'];
+
 //Create controller
-function schoolController($scope) {
-    $scope.message = "This is my message from schoolController";
-}
-function studentController($scope) {
-    $scope.message = "This is my message from studentController";
+function schoolController($scope, Validator) {
+    //Validator.checkNumber(2);
+    //$scope.message = Validator.checkNumber(1);
+    $scope.checkNumber = function () {
+        $scope.message = Validator.checkNumber(1);
+    }
+    $scope.num = 1;
 }
 
-function teacherController($scope) {
-    $scope.message = "This is my message from teacherController";
+//function Validator($window) {
+//    return {
+//        checkNumber: checkNumber
+//    }
+//    function checkNumber(input) {
+//        if (input % 2 == 0) {
+//            $window.alert("This is even");
+//        } else {
+//            $window.alert("This is odd");
+//        }
+//    }
+//}
+
+function Validator($window) {
+    return {
+        checkNumber: checkNumber
+    }
+    function checkNumber(input) {
+        if (input % 2 == 0) {
+            return "This is even";
+        } else {
+            return "This is odd";
+        }
+    }
 }
