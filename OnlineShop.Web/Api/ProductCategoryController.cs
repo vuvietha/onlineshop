6db0241778faf6c3b29d5e1f3928c8b6dc9e7bc2
Vu Viet Ha <havv56@gmail.com>
2018-05-09 13:36:42 +0700

    paging for list

--- OnlineShop.Web/Api/ProductCategoryController.cs
+++ OnlineShop.Web/Api/ProductCategoryController.cs
@@ -21,15 +21,36 @@ namespace OnlineShop.Web.Api
         {
             this._productCategoryService = productCategoryService;
         }
-
+        //[Route("getall")]
+        //public HttpResponseMessage Get(HttpRequestMessage request)
+        //{
+        //    return CreateHttpResponse(request, () =>
+        //    {
+        //        var model = _productCategoryService.GetAll();
+        //        var responseData = Mapper.Map<List<ProductCategoryViewModel>>(model);
+        //        HttpResponseMessage response = request.CreateResponse(HttpStatusCode.OK, responseData);
+        //        return response;
+        //    });
+        //}
         [Route("getall")]
-        public HttpResponseMessage Get(HttpRequestMessage request)
+        public HttpResponseMessage Get(HttpRequestMessage request, int page, int pageSize = 20)
         {
             return CreateHttpResponse(request, () =>
             {
                 var model = _productCategoryService.GetAll();
-                var listProductCategoryVM = Mapper.Map<List<ProductCategoryViewModel>>(model);
-                HttpResponseMessage response = request.CreateResponse(HttpStatusCode.OK, listProductCategoryVM);
+                var totalRow = model.Count();
+                var query = model.OrderByDescending(x => x.CreatedDate).Skip(page * pageSize).Take(pageSize);
+                var responseData = Mapper.Map<List<ProductCategoryViewModel>>(query);
+                PaginationSet<ProductCategoryViewModel> pagenationSet = new PaginationSet<ProductCategoryViewModel>()
+                {
+                    Items = responseData,
+                    Page = page,
+                    TotalPages = (int)Math.Ceiling((decimal)totalRow / pageSize),
+                    TotalCount = totalRow
+
+
+                };
+                HttpResponseMessage response = request.CreateResponse(HttpStatusCode.OK, pagenationSet);
                 return response;
             });
         }
--- OnlineShop.Web/app/components/product_categories/productCategoryListController.js
+++ OnlineShop.Web/app/components/product_categories/productCategoryListController.js
@@ -3,10 +3,24 @@
     productCategoryListController.$inject = ['$scope','apiService']
     function productCategoryListController($scope, apiService) {
         $scope.productCategories = [];
+        $scope.page = 0;
+        $scope.pagesCount = 0;
         $scope.getProductCategories = getProductCategories;
-        function getProductCategories() {
-            apiService.get('/api/productcategory/getall', null, function (result) {
-                $scope.productCategories = result.data;
+        function getProductCategories(page) {
+            page = page || 0;
+            var config = {
+                params: {
+                    page: page,
+                    pageSize: 2
+
+                }  
+
+            };
+            apiService.get('api/productcategory/getall', config, function (result) {
+                 $scope.productCategories = result.data.Items;
+                 $scope.page = result.data.Page;
+                 $scope.pagesCount = result.data.TotalPages;
+                 $scope.totalCount = result.data.TotalCount;
 
             }, function () {
                 console.log("Load product category failed");
